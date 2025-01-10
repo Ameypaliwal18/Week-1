0763be35936b5c818f21acb5fab1525f260928b7
Ameypaliwal18 <ameypaliwal13@gmail.com>
2025-01-10 16:23:49 +0530

    Harry Age

--- Day1/Level-1/HarryAge.java
+++ Day1/Level-1/HarryAge.java
@@ -1,16 +1,16 @@
 class HarryAge {
-	public static void main(String[] args){
-		
-		//Name of the Person
-		String name = "Harry";
-		
-		//Person's Birth Year and Current Year
-		int birthYear = 2000, currentYear = 2024;
-		
-		//Age of the Person
-		int age = (currentYear - birthYear);
-		
-		System.out.println(name + "'s age in " + currentYear + " is " + age);
-		
-	}
+    public static void main(String[] args){
+       
+        //Name of the Person
+        String name = "Harry";
+       
+        //Person's Birth Year and Current Year
+        int BirthYear = 2000, PresentYear = 2024;
+       
+        //Age of the Person
+        int Age = (PresentYear - BirthYear);
+       
+        System.out.println(name + "'s age in " + PresentYear + " is " + Age);
+       
+    }
 }
