c87994b058a2e15848595c4812d2f43bd95fe37e
Ameypaliwal18 <ameypaliwal13@gmail.com>
2025-01-10 18:42:17 +0530

    Centimeters to nches and feet

---
+++ Day1/Level-1/Cm_to_inches.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class Cm_to_inches {
+	public static void main(String[] args) {
+		
+		//Initializing scanner class object
+		Scanner input = new Scanner(System.in);
+		
+		//Taking input for height from the user
+		System.out.print("Enter your height(in cm): ");
+		int heightInCm = input.nextInt();
+		
+		//Converting cm to inches
+		double inches = heightInCm/2.54;
+		
+		//Converting inches into feet
+		double feet = inches/12;
+		
+		System.out.println("Your Height in cm is " + heightInCm + " while in feet is " + feet + " and inches in " + inches);
+		input.close();
+		
+	}
+}
