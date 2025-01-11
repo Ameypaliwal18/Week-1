55ca3d92b3d19850adaa73e63c664670f3ce399a
Ameypaliwal18 <ameypaliwal13@gmail.com>
2025-01-11 15:37:30 +0530

    Smallest Among Three

---
+++ Day2/Level-1/SmallestAmongThree.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+class SmallestAmongThree {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        // Taking the number1 as input from the user
+        System.out.print("Enter the number 1: ");
+        int number1 = scanner.nextInt();
+        
+        // Taking the number2 as input from the user
+        System.out.print("Enter the number 2: ");
+        int number2 = scanner.nextInt();
+        
+        // Taking the number3 as input from the user
+        System.out.print("Enter the number 3: ");
+        int number3 = scanner.nextInt();
+        
+        // Initialising boolean variable false
+        boolean isSmaller = false;
+
+        // Checking if the number 1 is smaller than other two or not
+        if ((number1 < number2)  && (number1 < number3)) {
+            isSmaller = true;
+        }
+        
+        // printing the output
+        System.out.println("Is the first number the smallest? " + isSmaller);
+        scanner.close();
+    }
+}
