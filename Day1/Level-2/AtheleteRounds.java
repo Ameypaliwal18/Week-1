9f79dec471627787c72ff1776bb637123996dbb5
Ameypaliwal18 <ameypaliwal13@gmail.com>
2025-01-11 13:54:28 +0530

    Athelete Round 5km

---
+++ Day1/Level-2/AtheleteRounds.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+class AtheleteRounds {
+    public static void main(String[] args) {
+        //Initializing the Scanner object
+        Scanner scanner = new Scanner(System.in);
+
+        // User input of side1 of triangle 
+        System.out.print("Enter side 1 of the triangle in meters: ");
+        double side1 = scanner.nextDouble();
+        
+        // User input of side 2 of triangle 
+        System.out.print("Enter side 2 of the triangle in meters: ");
+        double side2 = scanner.nextDouble();
+        
+        // User input of side3 of triangle 
+        System.out.print("Enter side 3 of the triangle in meters: ");
+        double side3 = scanner.nextDouble();
+
+        // Calculating the perimeter of triangle
+        double perimeter = side1 + side2 + side3;
+
+        // Calculate rounds to complete 5 km
+        double distanceToRun = 5000; // 5 km in meters
+        double rounds = distanceToRun / perimeter;
+
+        // Output
+        System.out.println("The total number of rounds the athlete will run is " + Math.ceil(rounds) + " to complete 5 km");
+        scanner.close();
+    }
+}
