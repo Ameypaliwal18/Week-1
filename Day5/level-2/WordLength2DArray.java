eeec536bd33013ff00d165ca9f333658a9acc190
Ameypaliwal18 <ameypaliwal13@gmail.com>
2025-01-17 11:11:36 +0530

    Minor Changes

--- Day5/level-2/WordLength2DArray.java
+++ Day5/level-2/WordLength2DArray.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class WordLengthArray {
+public class WordLength2DArray {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
